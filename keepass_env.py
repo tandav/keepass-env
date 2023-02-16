d92c4afa11700915b94c382477d23bc0fc900c61
Alexander Rodionov <tandav@tandav.me>
2023-02-17 02:34:05 +0400

    bump version v0.0.2 -> v0.0.3

--- keepass_env.py
+++ keepass_env.py
@@ -3,7 +3,7 @@ from collections.abc import Sequence
 
 from pykeepass import PyKeePass
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 
 def validate_ref(ref: str, prefix: str, sep: str, sep2: str) -> None:
